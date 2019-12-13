bb3dfb4fe4c0fda00bedeb67c19eb368d36d57a4
gshanmugaiah <gurusamy.shanmugaiah@ehealth.com>
2019-12-12 23:28:13 -0800

    Initial version

--- resolver.js
+++ resolver.js
@@ -7,5 +7,28 @@ module.exports = {
     Query: {
         users: () => fakeDB.users,
         user: ({id}) => _.find(fakeDB.users, {id}),
+    },
+    Mutation: {
+        addUser: ({user}) => {
+            fakeDB.users.push(user);
+            return {
+                success: true
+            };
+        },
+        updateUser: ({user}) => {
+            let oUser = _.find(fakeDB.users, {id: user.id});
+            if (oUser) {
+                oUser.fName = user.fName;
+                oUser.lName = user.lName;
+                oUser.dob = user.dob;
+                return {
+                    success: true
+                };
+            } else {
+                return {
+                    success: false
+                };
+            }
+        }
     }
 };
